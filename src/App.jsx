373f669bf386aba2ce36ed514660c7b0c22405dd
YamJr <yamgrg2058@gmail.com>
2023-07-02 17:47:58 +0545

    done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Footer from "./Component/Footer/Footer"
+
+function App() {
+
+  return (
+    <>
+    <div className="container">
+     <Footer/>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './Footer.css';
+ 
+function Footer(){
+    return(
+        <>
+        <div className="footer">
+            <div className="Useful-link">
+                <h4>Useful link</h4>
+                <ul>
+                   <li><a href="#">Yt</a></li>
+                   <li><a href="#">Yt</a></li>
+                   <li><a href="#">Yt</a></li>
+                   <li><a href="#">Yt</a></li>
+                </ul>
+            </div>
+            <div className="Medias">
+                <p>Hello</p>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Footer
