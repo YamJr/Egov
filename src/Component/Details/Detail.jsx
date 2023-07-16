dc2d435402a68748db5657fbcd4cf98bceac6d51
YamJr <yamgrg2058@gmail.com>
2023-07-16 12:46:28 +0545

    ongoing

--- src/Component/Details/Detail.jsx
+++ src/Component/Details/Detail.jsx
@@ -5,11 +5,57 @@ function Detail(){
     return(
         <>
         <div className="section">
-            <h4>government representative</h4>
-            <div className="card">
-                <p>Hello</p>
+            <div className="Govern">
+                <div className="gov-info">
+                <h4>government representatives</h4>
+                    <div className="card">  
+                <div className="card-1">
+                            <p>Hellp</p>
+                            <p>President</p>
+                            <p>AppleDog</p>
+                        </div>
+                        <div className="card-1">
+                            <p>Hellp</p>
+                            <p>President</p>
+                            <p>AppleDog</p>
+                        </div>
+                        <div className="card-1">
+                            <p>Hellp</p>
+                            <p>President</p>
+                            <p>AppleDog</p>
+                        </div>
+                        </div>
+                </div>
+
             </div>
-        </div>
+
+
+            <div className="contact-info">
+                <table className="Info">
+                    <thead>
+                        <tr>
+                            <th>Organization</th>
+                            <th>Contact</th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                       <tr>
+                        <td>Police</td>
+                        <td>100</td>
+                       </tr>
+                       <tr>
+                        <td>Ambulance</td>
+                        <td>102</td>
+                       </tr>
+                       <tr>
+                        <td>Fire</td>
+                        <td>101</td>
+                       </tr>
+                    </tbody>
+                    </table>
+            </div>
+                
+            </div>
         </>
     )
 }
