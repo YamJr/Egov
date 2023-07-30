37457336fcdd57671129609ceab3c050b20403e1
YamJr <yamgrg2058@gmail.com>
2023-07-30 12:39:24 +0545

    ongoing

--- src/Component/Details/Detail.jsx
+++ src/Component/Details/Detail.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import './Detail.css'
-import president from "../../images/president.jpg";
+import president from "../../images/president.jpg"
 import prime from "../../images/prime.jpg"
+import chief from "../../images/chief.jpg"
+import humla from "../../images/humla.jpg"
+import bangla from "../../images/bangla.jpg"
+import covid from "../../images/covid.jpg"
+import rules from "../../images/rules.jpg"
 
 function Detail(){
     return(
@@ -14,25 +19,22 @@ function Detail(){
                 <div className="card-1">
                             <img src={president} alt="president" className="my-images"/>
                             <p>President</p>
-                            <p>AppleDog</p>
+                            <p>Ram Chandra Poudel</p>
                         </div>
                         <div className="card-1">
                         <img src={prime} alt="prime" className="my-images"/>
-                            <p>Hellp</p>
-                            <p>President</p>
-                            <p>AppleDog</p>
+                            <p>Prime Minister</p>
+                            <p>Pushpa Kamal Dahal</p>
                         </div>
                         <div className="card-1">
-                        <img src={president} alt="president" className="my-images"/>
-                            <p>Hellp</p>
-                            <p>President</p>
-                            <p>AppleDog</p>
+                        <img src={chief} alt="chief" className="my-images"/>
+                            <p>Chief Secretary</p>
+                            <p>Dr. Baikuntha Aryal</p>
                         </div>
                         </div>
                 </div>
 
             </div>
-
 
             <div className="contact-info">
                 <p>Emergency Numbers:</p>
@@ -60,8 +62,46 @@ function Detail(){
                     </table>
             </div>
                 
-            </div>
-        </>
+             </div>   
+                <div className="aside">
+
+                <div className="aside-left">
+                            <div className="blog-article">
+                            <h3>Floods, landslide damage properties worth millions in Humla</h3>
+                                <img src={humla} alt="humla" className="blog-images"/>
+                                <div className="blog-content">
+                                <p>Personnel from all three security forces have reached the incident site but continuous rainfall has been causing difficulty, police say.</p>
+                            </div>
+                            </div>
+                            <div className="blog-article">
+                            <h3>Prime Minister Hasina offers Nepal the use of Bangladesh’s Payra port</h3>
+                                <img src={bangla} alt="bangla" className="blog-images"/>
+                                <div className="blog-content">
+                                <p>Bangladeshi leader made the offer to PM Dahal during their visit to Italy to attend the UN Food Summit.</p>
+                            </div>
+                            </div>
+                            <div className="blog-article">
+                            <h3>Nepal receives 702,720 doses of Covid vaccine</h3>
+                                <img src={covid} alt="covid" className="blog-images"/>
+                                <div className="blog-content">
+                                <p>The shots come from France. Select hospitals in Kathmandu and districts administer the vaccine.</p>
+                            </div>
+                            </div>
+                </div>
+
+                <div className="aside-center">
+                            <div className="blog-rules">
+                                <img src={rules} alt="rule" className="rules-img" />
+                                <h3>Govt unveils 188-point policies and programs for FY 2023/24</h3>
+                                
+                            </div>
+                    </div>
+                <div className="aside-right">
+    
+                </div>
+
+                </div>
+                </>
     )
 }
 export default Detail
