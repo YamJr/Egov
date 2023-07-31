c3ebb78b906f6ab716e992576adc341180ea81f4
YamJr <yamgrg2058@gmail.com>
2023-07-31 08:32:44 +0545

    ongoing

--- src/Component/Details/Detail.jsx
+++ src/Component/Details/Detail.jsx
@@ -7,6 +7,9 @@ import humla from "../../images/humla.jpg"
 import bangla from "../../images/bangla.jpg"
 import covid from "../../images/covid.jpg"
 import rules from "../../images/rules.jpg"
+import chitra from "../../images/chitra.jpg"
+import birgunj from "../../images/birgunj.jpg"
+import assembly from "../../images/assembly.jpg"
 
 function Detail(){
     return(
@@ -93,11 +96,40 @@ function Detail(){
                             <div className="blog-rules">
                                 <img src={rules} alt="rule" className="rules-img" />
                                 <h3>Govt unveils 188-point policies and programs for FY 2023/24</h3>
-                                
+                                <ul className="list">
+                                    <li>The government has decided to either cancel or merge the development committees and funds that have duplication of work, are not working according to the purpose, and are irrelevant.</li>
+                                    <li>The government has also revealed that a national identity card number is required for opening a bank account, getting a vehicle license, issuing house design approval and getting a permanent account number.</li>
+                                    <li>the government is going to make arrangements to allow Nepalese citizens abroad to vote in periodic elections. The security agencies will be empowered by using modern technology to control all types of criminal activities including terrorism and organized crime, money laundering, violence, cyber-crime, illegal drug trafficking</li>
+                                    <li>In the policy and program, the matters related to health include implementation of one doctor and one health institution, to operate OPD services from 8 AM to 8 PM, and building a state-of-the-art hospital that can treat heart and cancer diseases at the provincial level.</li>
+
+                                </ul>
                             </div>
                     </div>
+
+
                 <div className="aside-right">
-    
+                <div className="blog-article">
+                            <h3>Politics have been turned into a business: Chitra Bahadur</h3>
+                                <img src={chitra} alt="chitra" className="blog-images"/>
+                                <div className="blog-content">
+                                <p>Rashtriya Janamorcha Chairman Chitra Bahadur KC pinned blamed on the big political parties of the country…</p>
+                            </div>
+                            </div>
+                            <div className="blog-article">
+                            <h3>Birgunj Collects NPR 40 Billion in Customs Revenue!</h3>
+                                <img src={birgunj} alt="birgunj" className="blog-images"/>
+                                <div className="blog-content">
+                                <p>Nepal’s biggest Birgunj Customs Office has collected significant revenues in the fiscal year so far.</p>
+                            </div>
+                            </div>
+                            <div className="blog-article">
+                            <h3>Nepal’s Central Assembly Endorses Two Key Ordinances</h3>
+                                <img src={assembly} alt="assembly" className="blog-images"/>
+                                <div className="blog-content">
+                                <p>The National Assembly of Nepal endorsed two ordinances on Monday. Both these ordinances are finance-related.</p>
+                            </div>
+                            </div>
+
                 </div>
 
                 </div>
